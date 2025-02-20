18bc57db8d7155583fdbf94b39c37c736232614a
Cesia-PMartinez <yuniseu977@gmail.com>
2025-02-20 09:40:42 -0600

    ✨Deahgit add .

---
+++ script.js
@@ -0,0 +1,8 @@
+let element = document.getElementById("contenido");
+
+element.innerHTML = "<p>Cnosejos para que tengas una mejor clase con el tecaher Irvin</p>"
+
+let did1 = document.createElement("div");
+did1.innerHTML 
+
+
