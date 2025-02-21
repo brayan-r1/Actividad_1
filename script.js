292a8f816bd2080a6a5a0b26f3a690f2909ef6fe
brayan-r1 <brayan.rivas2026@hilasal.superate.org.sv>
2025-02-20 18:31:34 -0600

    Codigo Final

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 let element = document.getElementById("contenido");
 
-element.innerHTML = "<p>Cnosejos para que tengas una mejor clase con el tecaher Irvin</p>"
-
-let did1 = document.createElement("div");
-did1.innerHTML 
-
+const tema = document.createElement("h2");
+tema.innerHTML = "Mini manual para sobrevivir con el Teacher Irvin";
+tema.setAttribute("class", "titulo-manual");
+contenido.appendChild(tema);
+ 
+const consejos = [
+    "Practica todos los días",
+    "Toma notas mientras estudias",
+    "Haz preguntas si tienes dudas",
+    "Usa herramientas de estudio para tener un mejor conocimiento",
+    "Practica los códigos que has hecho para tener una mejor lógica",
+    "Repasa constantemente lo aprendido para afianzarlo.",
+    "Usa un vocabulario adecuado para dirigirte a los demás"
+];
+ 
+const lista = document.createElement("ul");
+lista.setAttribute("id", "lista-consejos");
+consejos.forEach(texto => {
+    let item = document.createElement("li");
+    item.innerHTML = texto;
+    lista.appendChild(item);
+});
+ 
+ 
+document.getElementById("contenido").appendChild(lista);
 
